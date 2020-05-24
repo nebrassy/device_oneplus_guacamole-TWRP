939c0daa14d9eb1f6b8bc847a5c1173826c5f8c5
Ralf Luther <luther.ralf@gmail.com>
2020-05-24 17:03:48 +0200

    guacamole: update and add overlay
    vendor.props:
    * add display properties from stock OOS
    * specify the device is the OverSeas version
    
    fwb:
    * specify a SSID name for our device specifically
    
    fwb AICP:
    * add integers for Sysinfo
    * specify some bools for LiveDisplay, although it is disabled
    
    PartsBin:
    * adapt paths and features to current status

--- vendor_props.mk
+++ vendor_props.mk
@@ -4,8 +4,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
+    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
+    ro.vendor.display.ad=1 \
+    ro.vendor.display.cabl=2 \
+    ro.vendor.display.sensortype=2 \
+    persist.vendor.color.matrix=2 \
+    vendor.display.comp_mask=0 \
+    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
+    vendor.display.disable_decimation=1 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_hw_recovery=0 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_inline_rotator=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.disable_ui_3d_tonemap=1 \
+    vendor.display.enable_null_display=0 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.display.enable_optimize_refresh=1 \
+    vendor.display.disable_mask_layer_hint=1 \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # build properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.oem.region=OverSeas \
     ro.vendor.build.release_type=release \
     ro.vendor.build.real.device=true \
     vendor.product.device=guacamole \
