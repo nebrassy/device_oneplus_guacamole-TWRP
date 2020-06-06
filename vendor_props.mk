b849335f06a4bd8abee8c64c859d695fea8332b1
Ralf Luther <luther.ralf@gmail.com>
2020-06-06 12:53:57 +0200

    guacamole: add surfaceflinger props for refresh rate switching
    Original author: "Park Ju Hyung <qkrwngud825@gmail.com>"

--- vendor_props.mk
+++ vendor_props.mk
@@ -35,6 +35,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.project_name=18821 \
     ro.vendor.boot.project_name=18821
 
+# ISurfaceflinger (refresh switchung)
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_smart_90_for_video=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
 # build properties
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.oem.region=OverSeas \
