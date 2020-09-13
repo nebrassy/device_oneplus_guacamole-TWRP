d169c2f25d28e85b97f2addb70d834975a6ba074
Ralf Luther <luther.ralf@gmail.com>
2020-09-14 00:33:08 +0200

    guacamole: just state ro.product.device correctly

--- vendor_props.mk
+++ vendor_props.mk
@@ -40,4 +40,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.release_type=release \
     ro.vendor.build.real.device=true \
     vendor.product.device=guacamole \
-    vendor.product.manufacturer=OPD
+    vendor.product.manufacturer=OPD \
+    ro.product.device=guacamole
