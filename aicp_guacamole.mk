726ce643b2391f58e3a8d76e90988c5bfcace2d7
Ralf Luther <luther.ralf@gmail.com>
2020-10-17 14:44:52 +0200

    guacamole: adjust maintainers for the moment
    Change-Id: Iaf96c5e11b09d419d66b1c9ead8e99f94e74986e

--- aicp_guacamole.mk
+++ aicp_guacamole.mk
@@ -41,7 +41,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Alex T. (GreekDragon), Julian Veit (Claymore1297), Ralf Luther (wartomato)"
+    DEVICE_MAINTAINERS="Julian Veit (Claymore1297), Ralf Luther (wartomato)"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
