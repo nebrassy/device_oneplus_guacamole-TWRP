91b769f77e0e1f141d049558b7dc3572e64595a5
Ralf Luther <luther.ralf@gmail.com>
2020-10-25 18:30:50 +0100

    guacamole: remove mixer_paths_11811.xml from device.mk
    Change-Id: I509caa7ecb9252505c9882a19a4d75e328508f38

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths_11811.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_11811.xml \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
 
