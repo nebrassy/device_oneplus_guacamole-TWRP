57d800d6ea26e1bfffe9ddfdac5a5d049e03f8d8
Ralf Luther <luther.ralf@gmail.com>
2021-05-15 16:57:18 +0200

    guacamole: remove old dax-default config from API29
    ...until we get this ready.
    Change-Id: I0582e67582a865da6dae6be02b1eb20ae33e8384

--- device.mk
+++ device.mk
@@ -29,9 +29,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
